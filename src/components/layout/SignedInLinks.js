89ceac57b1a2bf1bce5ba0c32c312f4e7922da02
footlessbird <>
2018-12-06 02:16:56 -0800

    first commit

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Post</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log Out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating orange lighten-2">
+          XX
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+const SignedOutLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">Signup</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Login</NavLink>
+      </li>
+      <li>
+       
+      </li>
+    </ul>
+  );
+};
+
+export default SignedOutLinks;
