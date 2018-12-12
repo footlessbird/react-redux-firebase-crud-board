d10a3263900d1d25f78ba12083c7bd7b7fbd8d59
footlessbird <>
2018-12-12 02:21:06 -0800

    181212 firebase configuration

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -2,7 +2,7 @@ export const CREATE_POST = "CREATE_POST";
 
 export const createPost = post => {
     //  due to redux-thunk can return function
-    return (dispatch, getSate) => {
+    return (dispatch, getSate,{getFirebase, getFirestore}) => {
     dispatch({ type: CREATE_POST, post });
   };
 };
