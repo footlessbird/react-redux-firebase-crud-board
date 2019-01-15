24e08b56aaaf719697d2222d75d95595482f7f97
footlessbird <>
2019-01-15 14:42:34 -0800

    having an issue with herokuapp changed to firebase

---
+++ dist/precache-manifest.2f2dc20a00aca225dc8a43380557999b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75c048fee058466f3886",
+    "url": "/static/js/main.75c048fe.chunk.js"
+  },
+  {
+    "revision": "e55193db22c4da8df256",
+    "url": "/static/js/1.e55193db.chunk.js"
+  },
+  {
+    "revision": "75c048fee058466f3886",
+    "url": "/static/css/main.a5cd8daf.chunk.css"
+  },
+  {
+    "revision": "e55193db22c4da8df256",
+    "url": "/static/css/1.d7508104.chunk.css"
+  },
+  {
+    "revision": "7e313af953318b04c39652348bd398e7",
+    "url": "/index.html"
+  }
+];
